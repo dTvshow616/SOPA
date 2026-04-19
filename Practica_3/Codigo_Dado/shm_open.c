9e7e8e9be6c67df3c114dfc4441dd7f7e82a30bd
Ana O.R. <ana.olsson@estudiante.uam.es>
2026-04-19 11:44:09 +0200

    Cosillas (voy a estar hasta las 13:00 o así haciendo commits)

--- Practica_3/Codigo_Dado/shm_open.c
+++ Practica_3/Codigo_Dado/shm_open.c
@@ -1,18 +1,37 @@
+#include <errno.h>      /* errno, EINTR */
+#include <fcntl.h>      /* manipulate file descriptor */
+#include <pthread.h>    /* POSIX threads */
+#include <semaphore.h>  /* Semaphores to manage the different processes*/
+#include <signal.h>     /* Signal handling */
+#include <stdint.h>     /* exact-width integer types */
+#include <stdio.h>      /* standard input/output library functions */
+#include <stdlib.h>     /* numeric conversion functions, pseudo-random numbers generation functions, memory allocation, process control functions */
+#include <sys/mman.h>   // TODO documentar
+#include <sys/stat.h>   // TODO documentar
+#include <sys/types.h>  // TODO documentar
+#include <sys/wait.h>   /* wait for process to change state */
+#include <time.h>       /* Tiempitos */
+#include <unistd.h>     /* POSIX operating system API */
+
+#define SHM_NAME "caca"
+
 /* This is only a code fragment, not a complete program... */
-fd_shm = shm_open(SHM_NAME, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
-if (fd_shm == -1) {
-  if (errno == EEXIST) {
-    fd_shm = shm_open(SHM_NAME, O_RDWR, 0);
-    if (fd_shm == -1) {
-      perror("Error opening the shared memory segment");
-      exit(EXIT_FAILURE);
+int main() {
+  int fd_shm = shm_open(SHM_NAME, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+  if (fd_shm == -1) {
+    if (errno == EEXIST) {
+      fd_shm = shm_open(SHM_NAME, O_RDWR, 0);
+      if (fd_shm == -1) {
+        perror("Error opening the shared memory segment");
+        exit(EXIT_FAILURE);
+      } else {
+        printf("Shared memory segment open\n");
+      }
     } else {
-      printf("Shared memory segment open\n");
+      perror("Error creating the shared memory segment\n");
+      exit(EXIT_FAILURE);
     }
   } else {
-    perror("Error creating the shared memory segment\n");
-    exit(EXIT_FAILURE);
+    printf("Shared memory segment created\n");
   }
-} else {
-  printf("Shared memory segment created\n");
 }
--- Practica_3/Ejercicio_7/src/monitor.c
+++ Practica_3/Ejercicio_7/src/monitor.c
@@ -7,17 +7,23 @@
  */
 
 /* La descripcion de las librerias a continuacion se ha sacado del man siempre que se podia */
-#include <errno.h>    /* errno, EINTR */
-#include <fcntl.h>    /* manipulate file descriptor */
-#include <pthread.h>  /* POSIX threads */
-#include <semaphore.h>/* Semaphores to manage the different processes*/
-#include <signal.h>   /* Signal handling */
-#include <stdint.h>   /* exact-width integer types */
-#include <stdio.h>    /* standard input/output library functions */
-#include <stdlib.h>   /* numeric conversion functions, pseudo-random numbers generation functions, memory allocation, process control functions */
-#include <sys/wait.h> /* wait for process to change state */
-#include <time.h>     /* Tiempitos */
-#include <unistd.h>   /* POSIX operating system API */
+#include <errno.h>      /* errno, EINTR */
+#include <fcntl.h>      /* manipulate file descriptor */
+#include <pthread.h>    /* POSIX threads */
+#include <semaphore.h>  /* Semaphores to manage the different processes*/
+#include <signal.h>     /* Signal handling */
+#include <stdint.h>     /* exact-width integer types */
+#include <stdio.h>      /* standard input/output library functions */
+#include <stdlib.h>     /* numeric conversion functions, pseudo-random numbers generation functions, memory allocation, process control functions */
+#include <sys/mman.h>   // TODO documentar
+#include <sys/stat.h>   // TODO documentar
+#include <sys/types.h>  // TODO documentar
+#include <sys/wait.h>   /* wait for process to change state */
+#include <time.h>       /* Tiempitos */
+#include <unistd.h>     /* POSIX operating system API */
+
+#define SHM_NAME "dos_puntos_tres"
+#define SEM_NAME "/miners_mutex" /* Semáforo para gestionar el acceso al fichero de mineros */
 
 int main(int argc, char* argv[]) {
   /* Retraso en milisegundoes de cada cosa */
@@ -29,6 +35,14 @@ int main(int argc, char* argv[]) {
 
   lag_comprobador = atoi(argv[1]);
   lag_monitor = atoi(argv[2]);
+  if (lag_comprobador < 0 || lag_monitor < 0) {
+    perror("Invalid arguments\n");
+    return -1;
+  }
+
+  comprobador();
+
+  return 0;
 }
 
 /**
@@ -53,8 +67,38 @@ void comprobador() {
  *
  */
 void monitor() {
+  int fd_shm; /* Descriptor del fichero */
+  sem_t* sem; /* Semáforo con nombre */
   /* Arranca en primer lugar y finaliza el último */
   /* Debe crear los segmentos de memoria compartida y los semáforos que compartirán los procesos del sistema */
+  fd_shm = shm_open(SHM_NAME, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+  if (fd_shm == -1) {
+    if (errno == EEXIST) {
+      fd_shm = shm_open(SHM_NAME, O_RDWR, 0);
+      if (fd_shm == -1) {
+        perror("Error opening the shared memory segment");
+        exit(EXIT_FAILURE);
+      } else {
+        printf("Shared memory segment open\n");
+      }
+    } else {
+      perror("Error creating the shared memory segment\n");
+      exit(EXIT_FAILURE);
+    }
+  } else {
+    printf("Shared memory segment created\n");
+  }
+
+  sem = sem_open(SEM_NAME, O_CREAT | O_EXCL, 0644, 1);
+  if (sem == SEM_FAILED) {
+    perror("sem_open");
+    close(fd_shm);
+    exit(EXIT_FAILURE);
+  }
+  // TODO: Meter el close(fd_shm);
+  // TODO: Meter el shm_unlink(SHM_NAME);
+  // TODO: Meter el sem_close(sem);
+  // TODO: Meter el sem_unlink(SEM_NAME);
   /* Si este proceso se para, el sistema detendrá la ejecución del Comprobador (?) */
   /* Si un minero arranca antes que este proceso, dará un mensaje de error y saldrá del sistema */
 }
